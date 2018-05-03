0df1f9b26fdcc166efc55ec6632089890ea6ac2b
korybrooks15 <38088273+korybrooks15@users.noreply.github.com>
2018-05-03 16:35:17 -0700

    Update profiles.h

--- profiles.h
+++ profiles.h
@@ -30,20 +30,20 @@ class Outgoing {
   if(personality == 2) {
     cout << " Congrats! You are CHARMING! " << endl;
     cout << "<><><><><><><><><><><><><><><>"
-    cout << "A suggested friend for you is..." << endl; //fill in the name
-    cout << "... is ... years old." << endl; //fill in pronoun and age
-    cout << "... is ... ft ... in" //fill in pronoun and height
-    cout << "... loves to ..." << endl; //fill in pronoun and short bio/interest/hobbies?
+    cout << "A suggested friend for you is KAMILAH_123<3!" << endl;
+    cout << "She is ... years old." << endl; //fill in age
+    cout << "She is ... ft ... in." //fill in height
+    cout << "She loves to ..." << endl; //fill in short bio/interest/hobbies?
     friend_request(); 
   }
   
   if(personality == 3) {
     cout << " Congrats! You are ADVENTUROUS! " << endl;
     cout << "<><><><><><><><><><><><><><><>"
-    cout << "A suggested friend for you is..." << endl; //fill in the name
-    cout << "... is ... years old." << endl; //fill in pronoun and age
-    cout << "... is ... ft ... in" //fill in pronoun and height
-    cout << "She loves to ..." << endl; //fill in pronoun and short bio/interest/hobbies?
+    cout << "A suggested friend for you is ERIC$$$!" << endl; 
+    cout << "He is ... years old." << endl; //fill in age
+    cout << "He is ... ft ... in." //fill in height
+    cout << "He loves to ..." << endl; //fill in pronoun and short bio/interest/hobbies?
     friend_request(); 
   }
   

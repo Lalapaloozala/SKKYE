0ba67cd01a0538ab5fe52c5d4343c8757b6faa57
korybrooks15 <38088273+korybrooks15@users.noreply.github.com>
2018-05-03 16:24:21 -0700

    Update and rename kory.cc to profiles.h

---
+++ profiles.h
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+
+void friend_request {
+  cout << "Enter a 1 if you would like to add them as a friend ";
+  cout << "or enter a 0 if you would not like to add them as a ";
+  cout << "friend" << endl;
+  if (answer == 1)
+    cout << "You currently have ! friend on SKKYE!" << endl;
+  if(answer == 0)
+    cout << "You currently have 0 fiends on SKKYE." << endl;
+}
+
+class Outgoing {
+  public:
+    string answer;
+  if(personality == 1) {
+    cout << " Congrats! You are outgoing! " << endl;
+    cout << "<><><><><><><><><><><><><><><>"
+    cout << "A suggested friend for you is..." << endl; //fill in the name
+    cout << "... is ... years old." << endl; //fill in pronoun and age
+    cout << "... is ... ft ... in" //fill in pronoun and height
+    cout << "She loves to ..." << endl; //fill in pronoun and short bio/interest/hobbies?
+    friend_request();    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+    

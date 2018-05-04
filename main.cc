8c45c4c9c1fb31603582302fa1770efa08282f20
Saleh Ghaffar <38123881+SalGx@users.noreply.github.com>
2018-05-04 00:05:12 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,3 +4,40 @@ using namespace std;
 int main() {
   
 }
+
+/* #include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+void die() {
+    cout << " ";
+    exit (0);
+}
+int main() {
+    Tree Bst;
+    vector <char> vec;
+    char letter;
+
+    while (cin){
+        cin >> letter;
+        if (isdigit(letter))
+            break;
+        else {
+            BST *temp = Bst.head;
+            Bst.insert(temp,letter);
+        }
+    }
+    while (cin) {
+        cin >> letter;
+        if (!cin) break;
+        if (!Bst.find(letter))
+            die();
+        vec.push_back(letter);
+    }
+    for (char c: vec)
+        Bst.print(c);
+    cout << "" << endl;
+}
+*/

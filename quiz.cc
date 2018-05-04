40c719d1211847b77a67144c01140bd155819891
Kamilah Murphy <38124831+Lalapaloozala@users.noreply.github.com>
2018-05-04 10:17:36 -0700

    Update quiz.cc

--- quiz.cc
+++ quiz.cc
@@ -36,13 +36,111 @@ int main() {
     cout << "of your ability using answer keys A, B, C, or D.\n";
     cout << endl;
     
-    cout << "1) What's your favorite color?\n";
+    cout << "1) What is your favorite color?\n";
     cout << "A) Yellow\n";
     cout << "B) Pink\n";
     cout << "C) Black\n";
     cout << "D) Blue\n";
     anwser();
     
+    cout << "2) What is your favorite genre of music?\n";
+    cout << "A) Rock\n";
+    cout << "B) Pop\n";
+    cout << "C) Electronic\n";
+    cout << "D) Hip-hop / Rap\n";
+    answer();
     
+    cout << "3) What is your favorite movie?\n";
+    cout << "A) Romantic\n";
+    cout << "B) Adventure\n";
+    cout << "C) Horror\n";
+    cout << "D) Comedy\n";
+    answer();
+    
+    cout << "4) What is your ideal weekend?\n";
+    cout << "A) Relaxing\n";
+    cout << "B) Hanging with friends\n";
+    cout << "C) Hiking\n";
+    cout << "D) Partying\n";
+    answer();
+    
+    cout << "5) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+    
+    cout << "6) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+    
+    cout << "7) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+    
+    cout << "8) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+    
+    cout << "9) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+    
+    cout << "10) What is your favorite
+    cout << "A)  
+    cout << "B) 
+    cout << "C) 
+    cout << "D) 
+    answer();
+
+5. FAV PET
+A CAT
+B BUNNY
+C REPTILE
+D DOG
+
+6. FAV SCHOOL SUBJECT
+A ART
+B MATH 
+C P.E.
+D ENGLISH
+
+7. WHERE WOULD YOU RATHER LIVE
+A SUBURBS
+B BEACH
+C COUNTRY
+D CITY
+
+8. FAV DRINK
+A TEA
+B WATER
+C ENERGY DRINKS
+D COFFEE
+
+9. WHICH CITY WOULD YOU RATHER VISIT
+A PARIS
+B AMSTERDAM
+C TOKYO
+D RIO
+
+10. FAV ANIMAL
+A ELEPHANT
+B SLOTH
+C SNAKE
+D LION
+
   
 }

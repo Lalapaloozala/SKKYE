8c80d0db06e856c8f23529cedc089a199d963939
yleiax <38294566+yleiax@users.noreply.github.com>
2018-05-04 11:00:57 -0700

    Update profiles.h

--- profiles.h
+++ profiles.h
@@ -48,7 +48,7 @@ class Outgoing {
   if(personality == 4) {
     cout << " Congrats! You are OUTGOING! " << endl;
     cout << "<><><><><><><><><><><><><><><>"
-    cout << "A suggested friend for you is YLEIAAA<3" << endl; //fill in the name
+    cout << "A suggested friend for you is YLEIA_XO" << endl; //fill in the name
     cout << "She is 21 years old." << endl; //fill in pronoun and age
     cout << "She is 5' tall.\n"; //fill in pronoun and height
     cout << "She loves to go to parties and hang out with friends!" << endl; //fill in pronoun and short bio/interest/hobbies?

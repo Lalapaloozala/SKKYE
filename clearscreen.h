8948e7e605bb68f45a41495b9bd9c7c0f53fc343
Saleh Ghaffar <38123881+SalGx@users.noreply.github.com>
2018-05-06 23:38:18 -0700

    Update clearscreen.h

--- clearscreen.h
+++ clearscreen.h
@@ -1,5 +1,6 @@
 //CLEAR SCREEN(may or may not implement in our project)
 //lterminfo (compiler flag)
+//Can be added to out personality quiz and also when our profiles and user prompts come up
 #include <term.h>
 #include <unistd.h>
 

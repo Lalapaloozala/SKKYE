f1093176dedf12d33a5d69aa1d759597d8bfa328
MrKingofspades <ericvining_70@yahoo.com>
2018-05-08 09:03:07 -0700

    Update Personal_Info.h

--- Personal_Info.h
+++ Personal_Info.h
@@ -3,40 +3,66 @@
 
 using namespace std;
 
-void die(){
-    cout <<"BAD INPUT\n";
+string first, last, username, password, password1;
+char correct;
+int x, y = 0;
+bool input = true;
+int tries = 0;
+
+void die() {
+    cout << "BAD INPUT\n";
     exit(0);
 }
 
-int main() {
-    string first, last;
-    string username, password;
-    char correct;
-    int x, y = 0;
-    bool input = true;
+void pass_die() {
+    cout << "If you can't even remember the password you just typed in, there is no point being on the internet..........Prepearing Doxing session.......Good luck :)\n";
+    exit(0);
+}
 
-    cout << "Enter your first name: ";
+void GetName() {
+    cout << "Sign up for SKKYE!" << endl;
+    cout << "\nEnter your first name: ";
     cin >> first;
+    for (int i = 0; i < first.size(); i++) {
+        if (!cin || !isalpha(first.at(i))) die();
+    }
+    if (islower(first.at(0))) {
+        first.at(0) = toupper(first.at(0));
+    }
+    cout << first << endl;
+
     cout << "\nEnter your last name: ";
     cin >> last;
-    cout << "\n";
-    cout << first + " " + last << endl;
+    for (int i = 0; i < last.size(); i++) {
+        if (!cin || !isalpha(last.at(i))) die();
+    }
+    if (islower(last.at(0))) {
+        last.at(0) = toupper(last.at(0));
+    }
 
+    cout << last << endl << endl;
+}
+
+void user_and_pass() {
+    cout << first + " " + last << ", please choose a username: ";
+    cin >> username;
+    if (!cin) die();
+    cout << "\nChoose a password: ";
+    cin >> password;
+    if (!cin) die();
+    cout << endl;
     while (input) {
-        cout << "Enter your height (feet first): ";
-        cin >> x;
-        if(!cin) die();
-        cout << "\nEnter your height (inches): ";
-        cin >> y;
-        if(!cin) die();
-        cout << "\nYour height is " << x << " foot and " << y << " inches. Is this correct? (Y or N): ";
-        cin >> correct;
-        if (correct == 'n' || correct == 'N'){
+        cout << "Please enter your password again (Yeah, we're gonna be like that): ";
+        cin >> password1;
+        if (!cin) die();
+        if (password1 != password){
+            cout << "Passwords do not match!\n";
+            tries++;
+                if(tries >= 3) { pass_die(); }
+                } else {
+            break;
         }
-            if (correct == 'y' || correct == 'Y'){
-                input = false;
     }
 }
-}
-                                            
 
+                                                                                                                                                                                                                           1,19          Top

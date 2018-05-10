e6c5e7eab6033e6bad0624232b9637c134909a1e
Kamilah Murphy <38124831+Lalapaloozala@users.noreply.github.com>
2018-05-10 16:34:12 -0700

    Update simpler_server.cc

--- simpler_server.cc
+++ simpler_server.cc
@@ -6,7 +6,7 @@
 #include "internet_server.h"
 
 int main() {
-    Internet_Server(7299,"Hi Kamilah");
-    Internet_Server(7299,"Hi Garrett");
+    Internet_Server(7299,"Hi");
+    Internet_Server(7299,"Hello");
     return 0;
 }
